41e38de10b5988c2453eb6bfe3c2e3ec31bb5815
HablSank <hablsankrumipradhab@gmail.com>
2025-11-14 20:35:36 +0700

    Styling Ekraf Details Section (DetailMisi.vue & DetailRewards.vue

--- resources/js/pages/DashboardEkraf/DetailMisi.vue
+++ resources/js/pages/DashboardEkraf/DetailMisi.vue
@@ -24,13 +24,13 @@ import { Link } from '@inertiajs/vue3';
 
 
 <template>
-    <div class="pb-32">
+    <div class="pb-32 bg-[#EBF5FF]">
         
-        <header class="p-4 flex items-center gap-14">
-            <Link href="/dashboard/ekraf/mission" class="text-gray-800">
-                <Icon icon="mdi:arrow-left" class="text-2xl"></Icon>
+        <header class="p-4 flex items-center gap-19">
+            <Link href="/dashboard/ekraf/mission" class="text-black">
+                <Icon icon="mdi:arrow-left" class="text-3xl"></Icon>
             </Link>
-            <h1 class="text-xl font-bold text-gray-900">
+            <h1 class="text-xl font-bold text-black">
                 Detail Misi
             </h1>
             
@@ -43,17 +43,19 @@ import { Link } from '@inertiajs/vue3';
             </div>
 
             <div class="space-y-2">
-                <p class="text-sm font-medium">Transaksi</p>
+                <p class="text-lg font-medium">Transaksi</p>
                 <h2 class="text-2xl font-bold">{{ detail.title }}</h2>
-                <p class="text-base text-gray-600">
+                <p class="text-base text-gray-700">
                    {{ detail.description }}
                 </p>
             </div>
 
-            <div class="bg-white rounded-xl shadow-xl p-4 space-y-3">
+            <div class="bg-[#D8EBFF] rounded-xl shadow-xl p-4 space-y-3 mb-16">
                 
                 <div class="flex items-center gap-3 pb-3 border-b border-gray-400">
-                    <Icon icon="tabler:target-arrow" class="text-3xl text-gray-500"></Icon>
+                    <div class="flex-shrink-0 rounded-b-full bg-white p-2">
+                        <Icon icon="tabler:target-arrow" class="text-3xl text-black"></Icon>
+                    </div>
                     <div class="flex-grow">
                         <p class="text-xs text-gray-500">Tipe Misi</p>
                         <p class="font-semibold text-gray-900">{{ detail.type }}</p>
@@ -61,7 +63,9 @@ import { Link } from '@inertiajs/vue3';
                 </div>
 
                 <div class="flex items-center gap-3 pb-3 border-b border-gray-400">
-                    <Icon icon="mingcute:calendar-2-fill" class="text-3xl text-gray-500"></Icon>
+                    <div class="flex-shrink-0 rounded-full bg-white p-2">
+                        <Icon icon="mingcute:calendar-2-fill" class="text-3xl text-black"></Icon>
+                    </div>
                     <div class="flex-grow">
                         <p class="text-xs text-gray-500">Tanggal Dibuat</p>
                         <p class="font-semibold text-gray-900">{{ formatTanggal(detail.created_at) }}</p>
@@ -69,7 +73,9 @@ import { Link } from '@inertiajs/vue3';
                 </div>
 
                 <div class="flex items-center gap-3">
-                    <Icon icon="el:star-alt" class="pl-1 text-2xl text-gray-500"></Icon>
+                    <div class="flex-shrink-0 rounded-t-full bg-white p-2">
+                        <Icon icon="el:star-alt" class="text-3xl text-black"></Icon>
+                    </div>
                     <div class="flex-grow">
                         <p class="text-xs text-gray-500">Poin Hadiah</p>
                         <p class="font-semibold text-gray-900">{{ detail.reward_points }} Poin</p>
@@ -77,12 +83,12 @@ import { Link } from '@inertiajs/vue3';
                 </div>
             </div>
 
-            <div class="bg-gray-200 rounded-xl shadow-sm p-4 flex flex-col items-center gap-4">
+            <div class="bg-white p-4 flex flex-col items-center gap-4">
                 <h2 class="text-base font-semibold text-gray-900">Kode QR Misi</h2>
                 <div class="w-48 h-48 bg-gray-100 flex items-center justify-center">
                     <img :src="`${url}/storage/${detail.qr_code_path}`" alt="qrcode misi" class="w-full">
                 </div>
-                <a :href="`${url}/storage/${detail.qr_code_path}`" download class="w-full bg-gray-300 text-gray-800 font-medium py-3 px-4 rounded-lg text-sm hover:bg-gray-400 transition-all flex items-center justify-center gap-2">
+                <a :href="`${url}/storage/${detail.qr_code_path}`" download class="w-full bg-[#01ABFF] text-white font-medium py-3 px-4 rounded-lg text-sm hover:bg-gray-400 transition-all flex items-center justify-center gap-2">
                     <Icon icon="material-symbols:download" class="text-xl"></Icon>
                     Unduh QR Code
                 </a>
@@ -90,9 +96,9 @@ import { Link } from '@inertiajs/vue3';
 
         </main>
     </div> 
-    <div class="fixed bottom-0 left-0 right-0 p-4 bg-gray-50 border-t border-gray-200">
+    <div class="fixed bottom-0 left-0 right-0 p-4">
         <div class="max-w-lg mx-auto">
-            <button class="w-full bg-gray-300 text-gray-800 font-semibold py-3 px-4 rounded-xl text-base hover:bg-gray-400 transition-all">
+            <button class="w-full bg-linear-to-r from-[#146AC7] to-[#75B7FD] text-white font-semibold py-3 px-4 rounded-xl text-base transition-all">
                 Nonaktifkan Misi
             </button>
         </div>
--- resources/js/pages/DashboardEkraf/DetailRewards.vue
+++ resources/js/pages/DashboardEkraf/DetailRewards.vue
@@ -23,38 +23,40 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="pb-32"> 
-        <header class="p-4 flex items-center gap-14">
-            <Link href="/dashboard/ekraf/reward" class="text-gray-800">
-                <Icon icon="mdi:arrow-left" class="text-2xl"></Icon>
+    <div class="pb-32 bg-[#EBF5FF]"> 
+        <header class="p-4 flex items-center gap-16">
+            <Link href="/dashboard/ekraf/reward" class="text-black">
+                <Icon icon="mdi:arrow-left" class="text-3xl"></Icon>
             </Link>
-            <h1 class="text-xl font-bold text-gray-900">
+            <h1 class="text-xl font-bold text-black">
                 Detail Hadiah
             </h1>
         </header>
 
         <main class="px-4 space-y-5">
             
-            <div class="w-full h-48 bg-gray-200 rounded-xl flex items-center justify-center">
+            <div class="bg-gray-200 rounded-xl flex items-center justify-center">
                 <img :src="`${url}/storage/${detail.reward_photo_path}`" alt="qrcode misi" class="w-100 rounded-lg">
             </div>
 
             <div class="space-y-2">
-                <p class="text-sm font-medium">{{ detail.type }}</p>
+                <p class="text-lg font-medium">{{ detail.type }}</p>
                 <h2 class="text-2xl font-bold">{{ detail.title }}</h2>
                 <div class="flex text-xl font-semibold gap-2">
-                <Icon icon="streamline:dollar-coin" class="text-3xl text-gray-500 relative bottom-0.5"></Icon>
+                <Icon icon="el:star-alt" class="text-3xl text-gray-500 relative bottom-0.5"></Icon>
                 <p>{{ detail.points_cost }} Poin</p>
                 </div>
-                <p class="text-base text-gray-600">
+                <p class="text-base text-gray-700">
                     {{ detail.description }}
                 </p>
             </div>
 
-            <div class="bg-white rounded-xl shadow-xl p-4 space-y-3">
+            <div class="bg-[#D8EBFF] rounded-xl shadow-xl p-4 space-y-3">
                 
                 <div class="flex items-center gap-3 pb-3 border-b border-gray-400">
-                    <Icon icon="tabler:target-arrow" class="text-3xl text-gray-500"></Icon>
+                    <div class="flex-shrink-0 rounded-b-full bg-white p-2">
+                        <Icon icon="tabler:target-arrow" class="text-3xl text-black"></Icon>
+                    </div>
                     <div class="flex-grow">
                         <p class="text-xs text-gray-500">Tipe Hadiah</p>
                         <p class="font-semibold text-gray-900">{{ detail.type }}</p>
@@ -62,7 +64,9 @@ import { Link } from '@inertiajs/vue3';
                 </div>
 
                 <div class="flex items-center gap-3 pb-3 border-b border-gray-400">
-                    <Icon icon="mingcute:calendar-2-fill" class="text-3xl text-gray-500"></Icon>
+                    <div class="flex-shrink-0 rounded-full bg-white p-2">
+                        <Icon icon="mingcute:calendar-2-fill" class="text-3xl text-black"></Icon>
+                    </div>
                     <div class="flex-grow">
                         <p class="text-xs text-gray-500">Tanggal di buat</p>
                         <p class="font-semibold text-gray-900">{{ formatTanggal(detail.created_at) }}</p>
@@ -70,7 +74,9 @@ import { Link } from '@inertiajs/vue3';
                 </div>
 
                 <div class="flex items-center gap-3">
-                    <Icon icon="el:star-alt" class="pl-1 text-2xl text-gray-500"></Icon>
+                    <div class="flex-shrink-0 rounded-t-full bg-white p-2">
+                        <Icon icon="el:star-alt" class="text-3xl text-black"></Icon>
+                    </div>
                     <div class="flex-grow">
                         <p class="text-xs text-gray-500">Harga Hadiah</p>
                         <p class="font-semibold text-gray-900">{{ detail.points_cost }} Poin</p>
@@ -80,9 +86,9 @@ import { Link } from '@inertiajs/vue3';
 
         </main>
     </div> 
-    <div class="fixed bottom-0 left-0 right-0 p-4 bg-gray-50 border-t border-gray-200">
+    <div class="fixed bottom-0 left-0 right-0 p-4">
         <div class="max-w-lg mx-auto">
-            <button class="w-full bg-gray-300 text-gray-800 font-semibold py-3 px-4 rounded-xl text-base hover:bg-gray-400 transition-all">
+            <button class="w-full bg-linear-to-r from-[#146AC7] to-[#75B7FD] text-white font-semibold py-3 px-4 rounded-xl text-base transition-all">
                 Nonaktifkan Hadiah
             </button>
         </div>
